079097ce75d4c96c8f8ee649fe44b46d39b25ab6
LazyJackyboy <skrok.jacek21@gmail.com>
2023-10-08 21:29:08 +0200

    Adding new files from Spring initlzr

---
+++ src/main/java/com/jacek/meety/MeetyApplication.java
@@ -0,0 +1,13 @@
+package com.jacek.meety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacek/meety/MeetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacek.meety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
