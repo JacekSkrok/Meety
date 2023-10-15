9c98fdaa23deee6f8e8ddc7ead36a41cf13c79a5
LazyJackyboy <skrok.jacek21@gmail.com>
2023-10-15 23:31:21 +0200

    Extend contollers with create, delete, update methods

--- src/main/java/com/jacek/meety/controllers/SessionsController.java
+++ src/main/java/com/jacek/meety/controllers/SessionsController.java
@@ -2,11 +2,9 @@ package com.jacek.meety.controllers;
 
 import com.jacek.meety.models.Session;
 import com.jacek.meety.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,4 +25,25 @@ public class SessionsController {
     public Session get(@PathVariable Long id) {
         return sessionRepository.getReferenceById(id);
     }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        //TODO: check if all data for Session were provided
+        Session existingSession = sessionRepository.getReferenceById(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
+
+
+
 }
