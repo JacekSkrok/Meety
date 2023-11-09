4f11975ce4717fac71fc6a34146ae1524abce554
LazyJackyboy <skrok.jacek21@gmail.com>
2023-11-09 18:04:33 +0100

    Added service for getting Appointments

--- frontend/meety/src/app/app.component.ts
+++ frontend/meety/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Appointment } from './appointment';
+import { AppointmentService } from './appointment.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public appointments: Appointment[];
   title = 'meety';
+
+  constructor( private appointmentService: AppointmentService ) {}
+
+  ngOnInit(): void{
+    this.getAppointments();
+  }
+
+  public getAppointments(): void{
+    this.appointmentService.getAppointments().subscribe(
+      (response: Appointment[]) => {
+        this.appointments = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
---
+++ frontend/meety/src/app/appointment.ts
@@ -0,0 +1,6 @@
+export interface Appointment {
+  id: number;
+  appointment_name: string;
+  description: string;
+  length: number;
+}
