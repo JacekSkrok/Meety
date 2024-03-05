89bf4f5f0cc16d5336f9b5c0436217740d5ac787
LazyJackyboy <skrok.jacek21@gmail.com>
2024-03-05 23:49:25 +0100

    Changing displaying Date and month in text format

--- frontend/meety/src/app/appointment.ts
+++ frontend/meety/src/app/appointment.ts
@@ -1,6 +1,10 @@
+import { Time } from "@angular/common";
+
 export interface Appointment {
   appointmentId: number;
   appointmentName: string;
+  appointmentDate: Date;
+  appointmentStartTime: Time;
   appointmentDescription: string;
   appointmentLength: number;
 }
