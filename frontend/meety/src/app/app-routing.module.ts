6c74d3b27e8ada6c8ac27978f961df965d7ffb6a
LazyJackyboy <skrok.jacek21@gmail.com>
2024-04-05 15:51:48 +0200

    Add sub directory for meet-page and add routing

--- frontend/meety/src/app/app-routing.module.ts
+++ frontend/meety/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { MeetComponent } from './meet-page/meet-page/meet-page.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'meeting/:id',
+    component: MeetComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
