e94ea86fcd15a4e044a0a4821253b54eed23b845
LazyJackyboy <skrok.jacek21@gmail.com>
2024-04-11 22:36:15 +0200

    Add new meet-page component

---
+++ frontend/meety/src/app/meet-page/meet-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meet-page',
+  templateUrl: './meet-page.component.html',
+  styleUrls: ['./meet-page.component.css']
+})
+export class MeetComponent {
+
+}
