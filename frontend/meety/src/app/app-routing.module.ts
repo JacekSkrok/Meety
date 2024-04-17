302ea68a45e496e611a5709a1bd1dfe3d4e828b6
LazyJackyboy <skrok.jacek21@gmail.com>
2024-04-17 23:05:18 +0200

    Redirect to /home path

--- frontend/meety/src/app/app-routing.module.ts
+++ frontend/meety/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { MeetComponent } from './meet-page/meet-page/meet-page.component';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { HostComponent } from './host/host.component';
@@ -9,7 +8,8 @@ import { HostComponent } from './host/host.component';
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent
+    redirectTo: '/home',
+    pathMatch: 'full'
   },
   {
     path: 'home',
