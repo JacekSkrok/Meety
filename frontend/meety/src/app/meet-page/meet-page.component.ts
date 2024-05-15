da0eea6bd5ed717835cb0e13999e74788175e9d0
LazyJackyboy <skrok.jacek21@gmail.com>
2024-05-15 16:36:54 +0200

    Rebuild meet-page.component

--- frontend/meety/src/app/meet-page/meet-page.component.ts
+++ frontend/meety/src/app/meet-page/meet-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-meet-page',
   templateUrl: './meet-page.component.html',
   styleUrls: ['./meet-page.component.css']
 })
-export class MeetComponent {
+export class MeetPageComponent {
+  constructor( private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      console.log('Meeting ID:', id);
+    });
+  }
 }
