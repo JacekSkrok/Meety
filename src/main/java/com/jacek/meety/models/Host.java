3adb5f542cc5dc4ab50d1b3ae038a3119c751f7c
LazyJackyboy <skrok.jacek21@gmail.com>
2024-06-13 16:59:26 +0200

    add findByUsername method

--- src/main/java/com/jacek/meety/models/Host.java
+++ src/main/java/com/jacek/meety/models/Host.java
@@ -4,16 +4,27 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.sql.Types;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Entity(name = "hosts")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-public class Host {
+public class Host implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long hostId;
+
+    private String username;
+    private String password;
+    private boolean enabled;
+
     private String firstName;
     private String lastName;
     private String title;
@@ -36,6 +47,44 @@ public class Host {
 
     public void setHostId(Long hostId) {
         this.hostId = hostId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password, PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(password);
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
     }
 
     public String getFirstName() {
@@ -92,5 +141,10 @@ public class Host {
 
     public void setHostPhoto(byte[] hostPhoto) {
         this.hostPhoto = hostPhoto;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
     }
 }
